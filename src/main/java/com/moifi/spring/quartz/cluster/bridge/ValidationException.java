111412eba991718c466b1390b0cbe5dc10056790
Rodrigo Duran Rodriguez <rodrigo@moifi.com>
2015-12-15 13:55:30 +0100

    add new Validation Exception

---
+++ src/main/java/com/moifi/spring/quartz/cluster/bridge/ValidationException.java
@@ -0,0 +1,27 @@
+package com.moifi.spring.quartz.cluster.bridge;
+
+/**
+ * Created by rodrigo on 15/12/15.
+ * Exception used in validations.
+ */
+public class ValidationException extends Exception {
+
+    public ValidationException() {
+    }
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidationException(Throwable cause) {
+        super(cause);
+    }
+
+    public ValidationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
